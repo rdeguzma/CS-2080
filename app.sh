4397414d4db35d428417e4b528fab3e17d356165
Ubuntu <ubuntu@ip-172-31-87-50.ec2.internal>
2025-04-10 15:12:02 +0000

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
-#service nginx start
+service nginx start
 #
-#sed -i "s/Welcome to nginx/Welcome to Reyes's page" "/var/www/html/index.nginx-debian.html"
+sed -i "s/Welcome to nginx/Welcome to Reyes's page" "/var/www/html/index.nginx-debian.html"
 #
-#sed -i "s/nginx/nginx (pronounced as EngineX)/g" "/var/www/html/index.nginx-debian.html"
+sed -i "s/nginx/nginx (pronounced as EngineX)/g" "/var/www/html/index.nginx-debian.html"
 #
-#service nginx restart
+service nginx restart
 #
 #
